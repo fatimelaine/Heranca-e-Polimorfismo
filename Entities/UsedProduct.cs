2f1d383f4f9a631af246b635e9f477c65887452c
“Elaine <fatima.elaine16@gmail.com>
2019-07-20 21:04:38 +0100

    Adicionados os primeiros arquivos do projeto

---
+++ Entities/UsedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioHerancaPolimorfismo.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag() //Iphone (used) $ 400.00 (Manufacture date: 15/03/2017)
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
